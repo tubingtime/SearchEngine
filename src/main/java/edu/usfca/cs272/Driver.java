cf77e2e2b76bb48f55d340c9cb75a037ba0a514a
tj <tjtrolling@gmail.com>
2022-09-09 14:44:57 -0700

    integrated Arg Parser

--- src/main/java/edu/usfca/cs272/Driver.java
+++ src/main/java/edu/usfca/cs272/Driver.java
@@ -8,7 +8,7 @@ import java.util.Arrays;
  * Class responsible for running this project based on the provided command-line
  * arguments. See the README for details.
  *
- * @author TODO Add Name Here
+ * @author Thomas de Laveaga
  * @author CS 272 Software Development (University of San Francisco)
  * @version Fall 2022
  */
@@ -24,29 +24,23 @@ public class Driver {
 		// store initial start time
 		Instant start = Instant.now();
 
-		// TODO Fill in and modify as needed
+
+		if (args.length < 1) {
+			args = new String[] {
+					"-max", "false", "-min", "0", "-min", "-10", "hello", "-@debug", "-f",
+					"output.txt", "-verbose" };
+		}
+		ArgumentParser argumentParser = new ArgumentParser(args);
+
 		System.out.println(Arrays.toString(args));
+		System.out.println(argumentParser.toString());
+
+
+
 
 		// calculate time elapsed and output
 		long elapsed = Duration.between(start, Instant.now()).toMillis();
 		double seconds = (double) elapsed / Duration.ofSeconds(1).toMillis();
 		System.out.printf("Elapsed: %f seconds%n", seconds);
 	}
-
-	/*
-	 * Generally, "Driver" classes are responsible for setting up and calling
-	 * other classes, usually from a main() method that parses command-line
-	 * parameters. Generalized reusable code are usually placed outside of the
-	 * Driver class. They are sometimes called "Main" classes too, since they
-	 * usually include the main() method.
-	 *
-	 * If the driver were only responsible for a single class, we use that class
-	 * name. For example, "TaxiDriver" is what we would name a driver class that
-	 * just sets up and calls the "Taxi" class.
-	 *
-	 * The starter code (calculating elapsed time) is not necessary. It can be
-	 * removed from the main method.
-	 *
-	 * TODO Delete this after reading.
-	 */
 }
