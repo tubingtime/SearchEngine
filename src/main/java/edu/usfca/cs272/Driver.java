20f01e6efdef023aae51f8d9d4f053e550231cff
tj <tjtrolling@gmail.com>
2022-09-12 18:37:21 -0700

    working on integration WordCleaner

--- src/main/java/edu/usfca/cs272/Driver.java
+++ src/main/java/edu/usfca/cs272/Driver.java
@@ -23,17 +23,17 @@ public class Driver {
 	public static void main(String[] args) {
 		// store initial start time
 		Instant start = Instant.now();
-
-
-		if (args.length < 1) {
-			args = new String[] {
-					"-max", "false", "-min", "0", "-min", "-10", "hello", "-@debug", "-f",
-					"output.txt", "-verbose" };
-		}
 		ArgumentParser argumentParser = new ArgumentParser(args);
 
 		System.out.println(Arrays.toString(args));
+		System.out.println(argumentParser.getPath("-text"));
 		System.out.println(argumentParser.toString());
+		try {
+			WordCleaner.listStems(argumentParser.getPath("-text"));
+		} catch (Exception e){
+			System.out.println(e);
+			return;
+		}
 
 
 
