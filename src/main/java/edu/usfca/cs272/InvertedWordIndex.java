4a31b9d6df3c1682e7c12a0ccf732d5a18179f7d
tj <tjtrolling@gmail.com>
2022-09-28 09:42:19 -0700

    got rid of WordObj and LocationsObj and use data structs directly! Much less code.

--- src/main/java/edu/usfca/cs272/InvertedWordIndex.java
+++ src/main/java/edu/usfca/cs272/InvertedWordIndex.java
@@ -29,107 +29,31 @@ import static edu.usfca.cs272.PrettyJsonWriter.*;
 
 public class InvertedWordIndex {
 
-	// TODO Undo some of the nested classes and just use the nested data structure directly
-	// TODO Have currently TreeMap<String, TreeMap<Path, ArrayList<Integer>>>
-	// TODO Change this to: TreeMap<String, TreeMap<String, TreeSet<Integer>>>
 	
 	/**
-	 ** Nested HashMap and HashSet data structure to store the locations and words
+	 ** Nested TreeMap and TreeSet data structure to store words and the locations where those words were found.
 	 */
-	private final TreeMap<String,WordObj> wIndex; /* Maybe i could use a hashset if I could do a custom hash func for just the word*/
-
-	/**
-	 * A data structure that holds a word along with where it was found
-	 */
-	public class WordObj implements Comparable<String>{
-		/**
-		 * Variable that stores the word this WordObj represents. It is the same as the Key in wIndex.
-		 * So this String probably just points to that and doesn't create a new instance?
-		 */
-		private final String word;
-
-		/**
-		 * Alphabetically sorted list of locations this word was found in.
-		 */
-		private final TreeMap<Path,LocationsObj> locations;
-
-		/**
-		 * Constructs a new instance of this class.
-		 */
-		private WordObj() {
-			this.word = null;
-			this.locations = new TreeMap<>();
-		}
-
-		/**
-		 * Constucts a new instance of this class with a word and adds the first location into the TreeMap
-		 * @param word the word this WordObj represents
-		 * @param location the location the word was found in
-		 */
-		private WordObj(String word, Path location) {
-			this.word = word;
-			this.locations = new TreeMap<>();
-			this.locations.put(location,new LocationsObj(location));
-		}
-
-		@Override
-		public int compareTo(String o) {
-			return this.word.compareTo(o);
-		}
-
-		/**
-		 * Uses the help of PrettyJSONWriter to convert a wordObj to JSON
-		 * @param writer the {@link Writer} to use
-		 * @param indent the level of indentation to use
-		 * @throws IOException if the Writer throws an IOException
-		 */
-		public void toJSON(Writer writer, int indent) throws IOException {
-			writer.write("{");
-			var iterator = locations.entrySet().iterator();
-			while (iterator.hasNext()){
-				Map.Entry<Path,LocationsObj> locObjEntry = iterator.next();
-				writer.write(newline);
-				writeIndent(writer,indent+1);
-				writeQuote(locObjEntry.getKey().toString(),writer,0);writer.write(": ");
-				writeArray(locObjEntry.getValue().lineLocations,writer, indent+1); //locationsObj.toJSON
-				if (iterator.hasNext()){writer.write(",");}
-			}
-			writer.write(newline);
-			writeIndent(writer,indent);
-			writer.write("}");
-		}
-	}
-
-	/**
-	 * Data structure to hold the file locations and line locations of a word.
-	 */
-	private class LocationsObj {
-		/**
-		 * the Path that this LocationsObj represents. It is the same as the Key of the TreeMap this obj is stored in.
-		 */
-		private final Path path;
-
-		/**
-		 * All the lines that the word was found at.
-		 */
-		public final ArrayList<Integer> lineLocations;
-
-		/**
-		 * Constructs a new LocationsObj and sets the path
-		 * @param path the path that this LocationsObj represents
-		 */
-		public LocationsObj(Path path) {
-			this.path = path;
-			this.lineLocations = new ArrayList<>();
-		}
-	}
+	private final TreeMap<String, TreeMap<String, TreeSet<Integer>>> wordMap;
 
 	/**
 	 ** Constructs a new instance of WordIndex.
 	 */
 	public InvertedWordIndex(){
-		this.wIndex = new TreeMap<>();
+		this.wordMap = new TreeMap<>();
 	}
+
+	/**
+	 * Constructs a new nested TreeMap that stores file locations and puts the first location in.
+	 * @param location the first location to put into the map
+	 * @return a nested TreeMap and TreeSet containing the location provided.
+	 */
+	public TreeMap<String, TreeSet<Integer>> newLocation(String location) {
+		TreeMap<String,TreeSet<Integer>> locationMap = new TreeMap<>();
+		TreeSet<Integer> lineLocations = new TreeSet<>();
+		locationMap.put(location, lineLocations);
+		return locationMap;
+	}
+
 
 	/**
 	 * Adds a new word to the WordIndex. Given the word, it's Path location, and the line number it was found at.
@@ -138,26 +62,49 @@ public class InvertedWordIndex {
 	 * @param line what line the word was found at
 	 */
 	public void add(String word, Path location, Integer line) {
-		wIndex.putIfAbsent(word,new WordObj(word,location));
-		wIndex.get(word).locations.putIfAbsent(location,new LocationsObj(location));
-		wIndex.get(word).locations.get(location).lineLocations.add(line); //so many dots o_o
+		String locationString = location.toString();
+		wordMap.putIfAbsent(word,newLocation(locationString));
+		wordMap.get(word).putIfAbsent(locationString,new TreeSet<>());
+		wordMap.get(word).get(locationString).add(line);
 	}
 
 	/**
-	 * Uses {@link WordObj} toJSON method to convert a wordIndex to JSON.
+	 * Uses wordToJSON toJSON method to convert a wordIndex to JSON.
 	 * @param writer the {@link Writer} to use
 	 * @param indent the level of indentation.
 	 * @throws IOException if the writer throws and IOException
 	 */
 	public void toJSON(Writer writer, int indent) throws IOException {
 		writer.write("{");
-		var iterator = wIndex.entrySet().iterator();
+		var iterator = wordMap.entrySet().iterator();
 		while (iterator.hasNext()){
-			Map.Entry<String, WordObj> wordObjEntry = iterator.next();
+			var wordEntry = iterator.next();
 			writer.write(newline);
 			writeIndent(writer,indent+1);
-			writeQuote(wordObjEntry.getKey().toString(),writer,0);writer.write(": ");
-			wordObjEntry.getValue().toJSON(writer, indent+1); //locationsObj.toJSON
+			writeQuote(wordEntry.getKey().toString(),writer,0);writer.write(": ");
+			wordToJSON(writer, indent+1, wordEntry.getValue()); //locationsObj.toJSON
+			if (iterator.hasNext()){writer.write(",");}
+		}
+		writer.write(newline);
+		writeIndent(writer,indent);
+		writer.write("}");
+	}
+
+	/**
+	 * Uses the help of PrettyJSONWriter to convert a wordObj to JSON
+	 * @param writer the {@link Writer} to use
+	 * @param indent the level of indentation to use
+	 * @throws IOException if the Writer throws an IOException
+	 */
+	public void wordToJSON(Writer writer, int indent, TreeMap<String, TreeSet<Integer>> locations) throws IOException {
+		writer.write("{");
+		var iterator = locations.entrySet().iterator();
+		while (iterator.hasNext()){
+			var locationEntry = iterator.next();
+			writer.write(newline);
+			writeIndent(writer,indent+1);
+			writeQuote(locationEntry.getKey().toString(),writer,0);writer.write(": ");
+			writeArray(locationEntry.getValue(),writer, indent+1);
 			if (iterator.hasNext()){writer.write(",");}
 		}
 		writer.write(newline);
